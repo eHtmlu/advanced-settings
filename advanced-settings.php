1b203d62c3e4321ee755bec48fed8fef2941393b
Helmut Wandl <helmut@ehtmlu.com>
2025-04-23 19:06:36 +0200

    Add comments

--- advanced-settings.php
+++ advanced-settings.php
@@ -217,11 +217,13 @@ function advset_is_admin_area() {
 	return is_admin() || in_array( $GLOBALS['pagenow'], array( 'wp-login.php', 'wp-register.php' ) );
 }
 
+// Get settings
 function advset_settings($id = null) {
 	$settings = get_option('advanced_settings_settings', []);
 	return $id ? (isset($settings[$id]) ? $settings[$id] : null) : $settings;
 }
 
+// Check if deprecated features are enabled
 function advset_show_deprecated_features() {
 	$show_deprecated = advset_settings('advset.features.show_deprecated');
 	return $show_deprecated && !empty($show_deprecated['enable']);
