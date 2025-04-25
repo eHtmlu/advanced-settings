ce49e770ef94e979f7a5bd13797e1a11a8f8ea2c
Helmut Wandl <helmut@ehtmlu.com>
2025-04-25 18:45:46 +0200

    Add migration of auto update setting

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
 add_action('plugins_loaded', function() {
     activate_plugin(plugin_basename(__DIR__ . '/advanced-settings.php'));
     deactivate_plugins(plugin_basename(__FILE__));
+
+	$auto_update_plugins = get_site_option('auto_update_plugins', []);
+	if ( in_array('advanced-settings/index.php', $auto_update_plugins) ) {
+		$auto_update_plugins[] = 'advanced-settings/advanced-settings.php';
+		update_site_option('auto_update_plugins', $auto_update_plugins);
+	}
 });
 
 require_once __DIR__ . '/advanced-settings.php';
