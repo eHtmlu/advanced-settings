8f2a5266d297ff90182b13eb818002f1e4bb6957
Helmut Wandl <helmut@ehtmlu.com>
2025-09-03 00:56:25 +0200

    Refactoring and optimizations

--- feature-setup/features/includes/frontend.auto_thumbs.php
+++ feature-setup/features/includes/frontend.auto_thumbs.php
@@ -3,12 +3,20 @@
 if (!defined('ABSPATH')) exit;
 
 
+/**
+ * Auto Thumbs feature class.
+ */
 class Advset__Feature__Auto_Thumbs {
 
     private static $instance = null;
 
     private function __construct() {}
     
+    /**
+     * Initialize the feature.
+     * 
+     * @return Advset__Feature__Auto_Thumbs The instance of the feature.
+     */
     public static function init() {
         if (self::$instance === null) {
             self::$instance = new self();
@@ -24,7 +32,7 @@ class Advset__Feature__Auto_Thumbs {
      * @param object $post The post object.
      * @return bool True if the post thumbnail was generated, false otherwise.
      */
-    public function transition_post_status( $new_status, $old_status, $post ) {
+    public function transition_post_status( string $new_status, string $old_status, object $post ) {
         global $wpdb;
         
         // If the post is not published, return
@@ -40,10 +48,10 @@ class Advset__Feature__Auto_Thumbs {
         $thumb_id = null;
     
         // Initialize variable used to store list of matched images as per provided regular expression
-        $images = array();
+        $images = [];
     
         // Get all images from post's body
-        preg_match_all('/<\s*img [^\>]*src\s*=\s*[\""\']?([^\""\'>]*)[^\>]*/i', empty($post->post_content) ? '' : $post->post_content, $images, PREG_SET_ORDER);
+        preg_match_all('/<\s*img [^\>]*src\s*=\s*[\""\']?([^\""\'>]*)[^\>]*/i', $post->post_content ?? '', $images, PREG_SET_ORDER);
     
         // If no images are found, return
         if (empty($images)) {
@@ -57,9 +65,9 @@ class Advset__Feature__Auto_Thumbs {
     
             // If the image is from wordpress's own media gallery, then it appends the thumbmail id to a css class.
             preg_match('/\s+class\s*=\s*[\""\']?([^\""\'>]*)/i', $img_html, $matches_class);
-            preg_match('/wp-image-([\d]*)/i', empty($matches_class[1]) ? '' : $matches_class[1], $matches_thumb_id);
-            if (!empty($matches_thumb_id[1]) && $matches_thumb_id[1] > 0) {
-                $thumb_id = $matches_thumb_id[1];
+            preg_match('/(?:^|\s+)wp-image-([\d]*)(?:\s+|$)/i', $matches_class[1] ?? '', $matches_thumb_id);
+            if (!empty($matches_thumb_id[1]) && (int) $matches_thumb_id[1] > 0) {
+                $thumb_id = (int) $matches_thumb_id[1];
                 break;
             }
     
@@ -70,8 +78,8 @@ class Advset__Feature__Auto_Thumbs {
     
             // If thumb id is not found, try to look for the image in DB. Thanks to "Erwin Vrolijk" for providing this code.
             $attachment = $wpdb->get_results($wpdb->prepare("SELECT ID FROM {$wpdb->posts} WHERE guid = %s", $image_url));
-            if (!empty($attachment[0]->ID) && $attachment[0]->ID > 0) {
-                $thumb_id = $attachment[0]->ID;
+            if (!empty($attachment[0]->ID) && (int) $attachment[0]->ID > 0) {
+                $thumb_id = (int) $attachment[0]->ID;
                 break;
             }
     
@@ -79,12 +87,11 @@ class Advset__Feature__Auto_Thumbs {
             preg_match('/\s+alt\s*=\s*[\""\']?([^\""\'>]*)/i', $img_html, $matches_alt);
             preg_match('/\s+title\s*=\s*[\""\']?([^\""\'>]*)/i', $img_html, $matches_title);
             $imported_thumb_id = $this->import_image_from_url($image_url, $post->ID, [
-                'alt' => empty($matches_alt[1]) ? '' : $matches_alt[1],
-                //'desc' => $image_title,
-                'title' => empty($matches_title[1]) ? '' : $matches_title[1],
+                'alt' => $matches_alt[1] ?? '',
+                'title' => $matches_title[1] ?? '',
             ]);
-            if (!is_wp_error($imported_thumb_id) && $imported_thumb_id > 0) {
-                $thumb_id = $imported_thumb_id;
+            if (!is_wp_error($imported_thumb_id) && (int) $imported_thumb_id > 0) {
+                $thumb_id = (int) $imported_thumb_id;
                 break;
             }
         }
@@ -107,62 +114,71 @@ class Advset__Feature__Auto_Thumbs {
      * @return int|\WP_Error Attachment-ID oder Fehler
      */
     private function import_image_from_url( string $url, int $post_id = 0, array $opts = [] ) {
-        // Validiere die URL
+        
+        // Validate the URL
         if (!wp_http_validate_url($url)) {
             return new WP_Error('invalid_url', __('Invalid image URL.'));
         }
 
+        // Parse the options
         $opts = wp_parse_args($opts, [
             'alt'     => '',
-            'desc'    => '',
             'title'   => '',
-            'author'  => get_current_user_id(),
-            'dedupe'  => true, // do not import the same source twice
         ]);
 
-        if ( $opts['dedupe'] ) {
-            $existing = get_posts([
-                'post_type'      => 'attachment',
-                'post_status'    => 'inherit',
-                'meta_key'       => '_source_url',
-                'meta_value'     => esc_url_raw($url),
-                'posts_per_page' => 1,
-                'fields'         => 'ids',
-            ]);
-            if ($existing) return (int) $existing[0];
-        }
-
+        // Require the necessary files
         require_once ABSPATH.'wp-admin/includes/file.php';
         require_once ABSPATH.'wp-admin/includes/image.php';
         require_once ABSPATH.'wp-admin/includes/media.php';
 
+        // Download the image
         $tmp = download_url($url);
-        if ( is_wp_error($tmp) ) return $tmp;
+        if ( is_wp_error($tmp) ) {
+            return $tmp;
+        }
 
+        // Get the name and mime type of the image
         $name = wp_basename(parse_url($url, PHP_URL_PATH) ?: 'image');
-        $mime = function_exists('wp_get_image_mime') ? wp_get_image_mime($tmp) : false;
+        $wp_filetype = wp_check_filetype_and_ext( $tmp, $name );
 
-        $file = [
+        // Check if the image is a valid image
+		if (empty($wp_filetype['type']) || !wp_match_mime_types('image', $wp_filetype['type'])) {
+			return new WP_Error('invalid_image', __('The uploaded file is not a valid image. Please try again.'));
+		}
+
+        // Create the file array
+        $file_array = [
             'name'     => $name,
             'tmp_name' => $tmp,
+            'type'     => $wp_filetype['type'],
         ];
-        if ($mime) $file['type'] = $mime;
 
-        $attachment_id = media_handle_sideload($file, $post_id, $opts['desc'], [
-            'post_title'  => $opts['title'] ?: preg_replace('/\.[^.]+$/', '', $name),
-            'post_author' => (int) $opts['author'],
-        ]);
+        // Create the post data
+        $post_data = [];
+        if (!empty($opts['title'])) {
+            $post_data['post_title'] = $opts['title'];
+        }
 
+        // Create the description
+        $desc = null;
+
+        // Sideload the image
+        $attachment_id = media_handle_sideload($file_array, $post_id, $desc, $post_data);
+
+        // Unlink the temporary file if it exists
+        @unlink($tmp);
+
+        // If there is an error, return the error
         if ( is_wp_error($attachment_id) ) {
-            @unlink($tmp);
             return $attachment_id;
         }
 
-        if ($opts['alt'] !== '') {
+        // Update the attachment meta if the alt text is provided
+        if (!empty($opts['alt'])) {
             update_post_meta($attachment_id, '_wp_attachment_image_alt', $opts['alt']);
         }
-        update_post_meta($attachment_id, '_source_url', esc_url_raw($url));
 
+        // Return the attachment ID (as integer)
         return (int) $attachment_id;
     }
 }
